6b2298597f0cb82c040fadde92d6d166a4e0bc92
2500030124 <vasudhadivyasuryadevara@gmail.com>
2026-02-24 20:49:26 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react'
 export default defineConfig({
-  plugins: [react()], 
-  base: "/e-comm-app/"
+  plugins: [react()],
+  base: '/ecomm-app/',   // 👈 Add this line
 })
